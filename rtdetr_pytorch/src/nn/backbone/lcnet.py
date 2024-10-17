42716c696d3557583de42931224285a1a72934aa
Shafa Meira Wahyono <meirashaf@gmail.com>
2024-10-17 21:46:14 +0700

    Update lcnet.py

--- rtdetr_pytorch/src/nn/backbone/lcnet.py
+++ rtdetr_pytorch/src/nn/backbone/lcnet.py
@@ -221,7 +221,7 @@ class LCNet(nn.Module):
         # self.dropout = nn.Dropout(p=dropout_prob)
         # self.flatten = nn.Flatten(start_dim=1, end_dim=-1)
 
-        self.fc = nn.Linear(self. class_expand, class_num)
+        # self.fc = nn.Linear(self. class_expand, class_num)
 
         if pretrained:
             # state = torch.hub.load_state_dict_from_url(MODEL_URLS[scale])
@@ -247,7 +247,7 @@ class LCNet(nn.Module):
         x = self.hardswish(x)
         # x = self.dropout(x)
         # x = self.flatten(x)
-        x = self.fc(x)
+        # x = self.fc(x)
         return x
 
 
