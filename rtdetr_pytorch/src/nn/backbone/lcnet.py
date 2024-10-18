c50e9dcfe424f9869208f1e4916baf24ac979c5f
Shafa Meira Wahyono <meirashaf@gmail.com>
2024-10-19 00:42:46 +0700

    Update lcnet.py

--- rtdetr_pytorch/src/nn/backbone/lcnet.py
+++ rtdetr_pytorch/src/nn/backbone/lcnet.py
@@ -228,8 +228,8 @@ class LCNet(nn.Module):
             padding=0,
             bias=False)
 
-        self.hardswish = nn.Hardswish()
-        self.dropout = nn.Dropout(p=dropout_prob)
+        # self.hardswish = nn.Hardswish()
+        # self.dropout = nn.Dropout(p=dropout_prob)
         # self.flatten = nn.Flatten(start_dim=1, end_dim=-1)
 
         # self.fc = nn.Linear(self. class_expand, class_num)
@@ -257,8 +257,8 @@ class LCNet(nn.Module):
         x = self.blocks6(x)
         # x = self.avg_pool(x)
         x = self.last_conv(x)
-        x = self.hardswish(x)
-        x = self.dropout(x)
+        # x = self.hardswish(x)
+        # x = self.dropout(x)
         # x = self.flatten(x)
         # x = self.fc(x)
         x = x.unsqueeze(-1)
